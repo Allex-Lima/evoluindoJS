43d1226e5c78bb7c510b50a65a3825b10b16a672
Alexandro <alexlmangabeira@gmail.com>
2022-09-29 22:09:21 -0300

    Corrigindo services/CreateUsuarioServices.ts

--- src/routers/usuarioRouter.ts
+++ src/routers/usuarioRouter.ts
@@ -1,23 +1,25 @@
 import { Router } from 'express';
 //import {v4 as uuidv4} from 'uuid'
 import { UsuarioRepository } from '../repository/UsuarioRepository';
+import { CreateUsuarioServices } from '../services/CreateUsuarioServices';
 
 export const usuarioRouter = Router();
-
-const novoUsuario = new UsuarioRepository();
+const usuarioRepository = new UsuarioRepository();
 
 usuarioRouter.post('/', (req, res) => {
     const { nome, email } = req.body;
     
-    novoUsuario.criar({ email, nome });
+    const createUsuarioServices = new CreateUsuarioServices(usuarioRepository);
+
+    createUsuarioServices.execute({ email, nome });
 
     return res.status(201).json({ message: 'Usuário criado com sucesso.' });
 });
 
 usuarioRouter.get('/', (req, res) => {
-    const listarTodosUsuarios = novoUsuario.listar();
+    const listarTodosUsuarios = usuarioRepository.listar();
     
-    const listaVazia = novoUsuario.listaVazia();
+    const listaVazia = usuarioRepository.listaVazia();
 
     if (listaVazia) {
         return res.status(201).json({ message: 'Lista vazia' });
@@ -29,7 +31,7 @@ usuarioRouter.get('/', (req, res) => {
 usuarioRouter.get('/:nome', (req, res) => {
     const { nome } = req.params;
 
-    const listarUsuarioId = novoUsuario.listarUsuarioId(nome);
+    const listarUsuarioId = usuarioRepository.listarUsuarioId(nome);
 
     if (listarUsuarioId === undefined) {
         return res.status(401).json({ message: 'Usuário não encontrado.' });
